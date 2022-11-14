54eb3d65c6c11b19072950dc6a7add3623398d6d
Yousuf Rabani <m.yousuf.rabani@gmail.com>
2022-11-14 11:17:22 -0800

    Add import statements for importing modules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import './style.css';
+import {pageElements} from './pageContent';
+
+const content = document.querySelector('#content');
+content.appendChild(pageElements());
 console.log("I am up and running!");
