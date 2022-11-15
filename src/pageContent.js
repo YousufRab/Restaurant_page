ce0d0b9df7a18de7330871c3552c751fa5a49f75
Yousuf Rabani <m.yousuf.rabani@gmail.com>
2022-11-15 10:52:37 -0800

    Create pageTabs function for generating html elements for home, menu and contact page link buttons

--- src/pageContent.js
+++ src/pageContent.js
@@ -1,21 +1,45 @@
 import shPic from './shakshouka.jpeg';
 
 export const pageElements = function () {
-    const contentDiv = document.querySelector('#content');
 
     const header = document.createElement('h1');
     header.innerHTML = "Sono in Calabria Bistro";
 
     const restText = document.createElement('p');
-    restText.innerHTML = "Come and join us at your local brunch house with a delicious new menu designed by our world class head chef, Vincenzo D'Agostini";
+    restText.innerHTML = "Come and join us at your fully-licensed local brunch house with a delicious new menu designed by our world class head chef Vincenzo D'Agostini";
+
+    const restInfo = document.createElement('p');
+    restInfo.innerHTML = "Open 7 days a week, 9am to 10pm. 125 Granville Street, Vancouver"
 
     const foodPic = new Image();
     foodPic.src = shPic;
     foodPic.classList.add('foodImg');
 
-    header.append(restText, foodPic);
+    header.append(restText, restInfo, foodPic);
 
-    contentDiv.appendChild(header);
+  
     
-    return contentDiv;
+    return header;
+}
+
+export const pageTabs = function () {
+    
+    const tabHolder = document.createElement('div');
+    tabHolder.classList.add('tabContainer');
+    
+    const homeBtn = document.createElement('div');
+    homeBtn.classList.add('home');
+    homeBtn.innerHTML = "Home |";
+
+    const menuBtn = document.createElement('div');
+    menuBtn.classList.add('menu');
+    menuBtn.innerHTML = "Menu |";
+
+    const contactBtn = document.createElement('div');
+    contactBtn.classList.add('contact');
+    contactBtn.innerHTML = "Contact";
+    
+    tabHolder.append(homeBtn, menuBtn, contactBtn);
+
+    return tabHolder;
 }
