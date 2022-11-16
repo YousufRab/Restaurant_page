859e2eb37e50a923e181e16499fc92ac62c3d29d
Yousuf Rabani <m.yousuf.rabani@gmail.com>
2022-11-16 11:37:12 -0800

    Create item Img element and import the img to be used

--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,4 @@
-
+import brusc from './Bruschetta.jpg';
 
 const menuPage = function() {
 
@@ -23,7 +23,19 @@ const menuPage = function() {
     const item1Name = document.createElement('div');
     item1Name.innerHTML = "Bruschetta";
     item1Name.classList.add('menuItemName');
+    const item1Img = new Image();
+    item1Img.src = brusc;
+    item1Img.classList.add('menuImage');
     const item1 = document.createElement('p');
     item1.classList.add('menuItemText');
+    item1.innerHTML = "Marinated cherry tomatoes, black olives, mozzarella and olive oil on toasted baguette slices";
+    brunchItem1.append(item1Name, item1Img, item1);
 
+
+
+
+
+
+
+    return menuHeader;
 }
