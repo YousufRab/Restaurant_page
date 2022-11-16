190c656437858189e928668e108b7ccb8b834e2d
Yousuf Rabani <m.yousuf.rabani@gmail.com>
2022-11-16 11:00:08 -0800

    Create further div elements for menu page in the menuPage function

--- src/menu.js
+++ src/menu.js
@@ -1,16 +1,29 @@
+
+
 const menuPage = function() {
+
+    const menuHeader = document.createElement('h1');
+    menuHeader.innerHTML = "Menu";
+
+    menuHeader.append(menuContainer);
 
     const menuContainer = document.createElement('div');
     menuContainer.id = "menuCon";
 
-    const brunchContainer = document.createElement('div');
+    const brunchContainer = document.createElement('div'); //append brunch items to this
     brunchContainer.id = "brunchCon";
 
     menuContainer.append(brunchContainer);
 
     const brunchHeader = document.createElement('div');
     brunchHeader.innerHTML = "Breakfast and Brunch items";
+    brunchContainer.append(brunchHeader);
 
-    
+    const brunchItem1 = document.createElement('div');
+    const item1Name = document.createElement('div');
+    item1Name.innerHTML = "Bruschetta";
+    item1Name.classList.add('menuItemName');
+    const item1 = document.createElement('p');
+    item1.classList.add('menuItemText');
 
 }
