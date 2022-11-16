643e8113efbcbf7d77bf502ade45626131f382c2
Yousuf Rabani <m.yousuf.rabani@gmail.com>
2022-11-16 10:31:00 -0800

    Add code for creating of initial container divs and header to menuPage function

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,16 @@
 const menuPage = function() {
 
     const menuContainer = document.createElement('div');
+    menuContainer.id = "menuCon";
+
+    const brunchContainer = document.createElement('div');
+    brunchContainer.id = "brunchCon";
+
+    menuContainer.append(brunchContainer);
+
+    const brunchHeader = document.createElement('div');
+    brunchHeader.innerHTML = "Breakfast and Brunch items";
+
+    
 
 }
