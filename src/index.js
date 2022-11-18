4ac42fd288cad69ee968cc69a9253281741a1cba
Yousuf Rabani <m.yousuf.rabani@gmail.com>
2022-11-18 10:09:24 -0800

    Add line of code to contactPageLoad that appends contactPage() to content div

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import './style.css';
 import {pageElements, pageTabs} from './pageContent';
 import {menuPage} from './menu';
+import { contactPage } from './contact';
 
 const content = document.querySelector('#content');
 
-content.append(pageTabs(), menuPage());
+content.append(pageTabs(), pageElements());
 
 const menuPageLoad = () => {
 
@@ -15,5 +16,29 @@ const menuPageLoad = () => {
 
 }
 
+const homePageLoad = () => {
+
+    const menuHeader = document.querySelector('#menuPageHeader');
+    menuHeader.remove();
+
+    content.append(pageElements());
+}
+
+const contactPageLoad = () => {
+
+    const header = document.querySelector('#homePage');
+    const menuHeader = document.querySelector('#menuPageHeader');
+    header.remove();
+    menuHeader.remove();
+
+    content.append(contactPageLoad());
+}
+
+const homeBtn = document.querySelector('.home');
+homeBtn.addEventListener('click', homePageLoad)
+
 const menuBtn = document.querySelector('.menu');
 menuBtn.addEventListener('click', menuPageLoad);
+
+const contactBtn = document.querySelector('.contact');
+contactBtn.addEventListener('click', contactPageLoad);
