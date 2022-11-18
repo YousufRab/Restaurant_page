c5a9c70d902d1fb7678d6a66c8d5c25fbe316974
Yousuf Rabani <m.yousuf.rabani@gmail.com>
2022-11-18 10:20:08 -0800

    Append hours and email element to contactHeader. Change contact header creation from div to h1 element

--- src/contact.js
+++ src/contact.js
@@ -1,6 +1,6 @@
 export const contactPage = () => {
 
-    const contactHeader = document.createElement('div');
+    const contactHeader = document.createElement('h1');
     contactHeader.innerHTML = "Contact";
     contactHeader.id = 'contactPageHeader';
 
@@ -11,9 +11,11 @@ export const contactPage = () => {
 
     const hours = document.createElement('p');
     hours.innerHTML = '9am - 10pm everyday';
+    contactContainer.append(hours);
 
     const email = document.createElement('p');
-    email.innerHTML = 'info@Sono.ca';
+    email.innerHTML = 'info@Sono.com';
+    contactContainer.append(email);
 
 
     return contactHeader;
